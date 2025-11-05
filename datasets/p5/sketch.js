beafdd6a303610c9582a4eaefd05520c91ffdd65
Thiago Hersan <thiago.hersan+github@gmail.com>
2025-11-04 22:08:07 -0500

    new fonts and new font csv files

--- datasets/p5/sketch.js
+++ datasets/p5/sketch.js
@@ -7,61 +7,72 @@ function isDigit(i) {
 function isLetter(i) {
   return inRange(i, 65, 90) || inRange(i, 97, 122);
 }
+function dig(v, digits) {
+  return parseFloat(v.toFixed(digits));
+}
 
-function exportAlphabet(font, fontname, numPoints = 100) {
+function exportAlphabet(p5font, font, sampleFactor = 32.0, fontSize = 48) {
   textAlign(CENTER, CENTER);
-
-  const initSF = parseInt(numPoints / 100);
-  const ttpOptions = { sampleFactor: initSF };
-  const fontSize = 48;
-
-  let csvStr = "";
+  const alphabet = [];
 
   for (let i = 48; i < 123; i++) {
     if (isDigit(i) || isLetter(i)) {
-      const mchar = String.fromCharCode(i);
+      const char = String.fromCharCode(i);
+      const points = p5font.textToPoints(char, 0, 0, fontSize, { sampleFactor }).map(({x,y}) => [dig(x, 6), dig(y, 6)]);
 
-      ttpOptions.sampleFactor = initSF;
-      let points = font.textToPoints(mchar, 0, 0, fontSize, ttpOptions);
-
-      while (abs(points.length - numPoints) > 2) {
-        if (points.length < numPoints) {
-          ttpOptions.sampleFactor *= 1.02;
-        } else {
-          ttpOptions.sampleFactor *= 0.98;
-        }
-
-        points = font.textToPoints(mchar, 0, 0, fontSize, ttpOptions);
-      }
-
-      csvStr += `${fontname},${mchar}`;
-
-      for (let p of points) {
-        csvStr += `,${p.x},${p.y}`;
-      }
-      csvStr += "\n";
+      alphabet.push({
+        font,
+        char,
+        points,
+      });
     }
   }
-
-  console.log(`${fontname} done ${int(millis() / 1000)}`);
-  return csvStr;
+  console.log(`${font} done ${int(millis() / 1000)}`);
+  return alphabet;
 }
 
 const FONTS = {
-  baskerville: "./fonts/LibreBaskerville-Regular.ttf",
-  cinzel: "./fonts/Cinzel-Regular.ttf",
-  garamond: "./fonts/EBGaramond-Regular.ttf",
-  habibi: "./fonts/Habibi-Regular.ttf",
-  inter: "./fonts/Inter-Regular.ttf",
-  lato: "./fonts/Lato-Regular.ttf",
-  montserrat: "./fonts/Montserrat-Regular.ttf",
-  newsreader: "./fonts/Newsreader-Regular.ttf",
-  notosans: "./fonts/NotoSans-Regular.ttf",
-  opensans: "./fonts/OpenSans-Regular.ttf",
-  playfair: "./fonts/PlayfairDisplay-Regular.ttf",
-  ptserif: "./fonts/PTSerif-Regular.ttf",
-  roboto: "./fonts/Roboto-Regular.ttf",
-  ubuntu: "./fonts/Ubuntu-Regular.ttf",
+  aubrey: "Aubrey-Regular.ttf",
+  baskerville: "LibreBaskerville-Regular.ttf",
+  bodoni: "BodoniModa_48pt-Regular.ttf",
+  cinzel: "Cinzel-Regular.ttf",
+  dmserif: "DMSerifText-Regular.ttf",
+  exo: "Exo-Regular.ttf",
+  faculty: "FacultyGlyphic-Regular.ttf",
+  fira: "FiraSans-Regular.ttf",
+  funnel: "FunnelSans-Regular.ttf",
+  garamond: "EBGaramond-Regular.ttf",
+  germania: "GermaniaOne-Regular.ttf",
+  glassantiqua: "GlassAntiqua-Regular.ttf",
+  googlecode: "GoogleSansCode-Regular.ttf",
+  grenze: "Grenze-Regular.ttf",
+  habibi: "Habibi-Regular.ttf",
+  ibmmono: "IBMPlexMono-Regular.ttf",
+  ibmsans: "IBMPlexSans-Regular.ttf",
+  ibmserif: "IBMPlexSerif-Regular.ttf",
+  inconsolata: "Inconsolata-Regular.ttf",
+  inter: "Inter-Regular.ttf",
+  lato: "Lato-Regular.ttf",
+  merriweather: "Merriweather_48pt-Regular.ttf",
+  montserrat: "Montserrat-Regular.ttf",
+  newsreader: "Newsreader-Regular.ttf",
+  notosans: "NotoSans-Regular.ttf",
+  opensans: "OpenSans-Regular.ttf",
+  oxanium: "Oxanium-Regular.ttf",
+  playfair: "PlayfairDisplay-Regular.ttf",
+  poppins: "Poppins-Regular.ttf",
+  ptserif: "PTSerif-Regular.ttf",
+  roboto: "Roboto-Regular.ttf",
+  rubik: "Rubik-Regular.ttf",
+  sansation: "Sansation-Regular.ttf",
+  slabo: "Slabo27px-Regular.ttf",
+  tasaexplorer: "TASAExplorer-Regular.ttf",
+  tiktoksans: "TikTokSans-Regular.ttf",
+  tinos: "Tinos-Regular.ttf",
+  ubuntu: "Ubuntu-Regular.ttf",
+  vendsans: "VendSans-Regular.ttf",
+  zain: "Zain-Regular.ttf",
+  zalando: "ZalandoSans-Regular.ttf",
 };
 
 let startButt, saveButt;
@@ -81,25 +92,24 @@ function setup() {
   saveButt.hide();
 }
 
-let csvResult = "font,char";
-const NUM_POINTS = 384;
+const allLetters = [];
 
-function startProcessing() {
+async function startProcessing() {
   startButt.hide();
 
-  for (let i = 0; i < NUM_POINTS + 2; i++) {
-    csvResult += `,x${i},y${i}`;
-  }
-  csvResult += "\n";
-
   for (const fontname in FONTS) {
-    loadFont(FONTS[fontname], (font) => {
-      csvResult += exportAlphabet(font, fontname, NUM_POINTS);
-    });
+    const font = await loadFont(`./fonts/${FONTS[fontname]}`);
+    allLetters.push(...exportAlphabet(font, fontname, 40.0));
   }
+
+  const lens = allLetters.map(l => l["points"].length);
+  console.log(allLetters.length);
+  console.log(Math.min(...lens), Math.max(...lens));
+
   saveButt.show();
 }
 
 function saveResults() {
-  saveStrings([csvResult], `fonts_${NUM_POINTS}_p5_raw.csv`);
+  // saveJSON(allLetters, "fonts_p5_raw.json");
+  saveStrings([JSON.stringify(allLetters)], "fonts_p5_raw.json");
 }
