4fa664d1c7e9a7b63544cb7e5750a3bbdaff4483
Thiago Hersan <thiago.hersan+github@gmail.com>
2025-11-15 20:24:59 -0500

    subset bird sounds. use about 1500 in 70/30 split

--- datasets/audio/birds/prep_birds.py
+++ datasets/audio/birds/prep_birds.py
@@ -1,5 +1,5 @@
 from os import listdir, makedirs, path, remove
-from random import shuffle
+from random import seed, shuffle
 from shutil import copy2, move, rmtree
 
 # copy files from test/val to train
@@ -25,24 +25,19 @@ for df in ["./birds/train", "./birds/test", "./birds/val", "./birds/background",
   if path.isfile(df):
     remove(df)
 
-# rename files
-dirs = sorted([d for d in listdir(f"./birds") if path.isdir(f"./birds/{d}")])
-for d in dirs:
-  fnames = sorted([f for f in listdir(f"./birds/{d}") if f.endswith("wav")])
-  for idx,f in list(enumerate(fnames)):
-    str_idx = f"000{idx}"[-4:]
-    fpath = f"./birds/{d}/{f}"
-    move(fpath, f"./birds/{d}/{d}_{str_idx}.wav")
-
-# prepare train/test for export
+# separate train/test for export
 makedirs("./birds/train", exist_ok=True)
 makedirs("./birds/test", exist_ok=True)
+seed(1010)
 
+# rename files
 dirs = sorted([d for d in listdir(f"./birds") if path.isdir(f"./birds/{d}") and d not in ["train", "test"]])
 for d in dirs:
-  fnames = [f for f in listdir(f"./birds/{d}") if f.endswith("wav")]
+  fnames = sorted([f for f in listdir(f"./birds/{d}") if f.endswith("wav")])[:210:2]
   shuffle(fnames)
-  for idx,f in enumerate(fnames):
-    split = "train" if idx < (0.8 * len(fnames)) else "test"
-    move(f"./birds/{d}/{f}", f"./birds/{split}/{f}")
+  for idx,f in list(enumerate(fnames)):
+    split = "train" if idx < (0.666 * len(fnames)) else "test"
+    str_idx = f"000{idx}"[-4:]
+    fpath = f"./birds/{d}/{f}"
+    move(fpath, f"./birds/{split}/{d}_{str_idx}.wav")
   rmtree(f"./birds/{d}")
